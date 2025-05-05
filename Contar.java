4e2db006f625b51bb0fe6560b04eec0c5bea7a4c
11041990 <vela3719diaz@gmail.com>
2025-05-04 18:41:02 -0600

    CONTAR NUMEROS PARES E IMPARES MARIO DE JESUS DIAZ VELA
    MARIO DE JESUS DIAZ VELA

---
+++ Contar.java
@@ -0,0 +1,23 @@
+
+
+  import java.util.Scanner;
+  public class Contar { 
+  public static void main(String[] args) {
+  
+    int cant, num, pares=0, impares=0;
+    System.out.print("Cuantos numeros desea ingresar: ");
+    Scanner scanner = new Scanner(System.in);
+    cant = scanner.nextInt();
+    
+    for(int i=1; i<=cant; i++){
+     System.out.print("Ingresar numero " + i + " de " + cant + ": ");
+      num = (new Scanner(System.in)).nextInt();
+       if(num%2 == 0)
+         pares++;
+       else
+         impares++; }
+         
+    System.out.println("\nCantidad de numeros pares: " + pares);
+    System.out.println("Cantidad de numeros impares: " + impares); } }
+   
+
